222eee2b255fd00ff7b58e3a1ac1cb6b321ecfaa
monicajaimesc <monikmaja@gmail.com>
2019-12-18 19:45:24 +0000

    init, delete and save

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,52 @@
 #!/usr/bin/python3
 """
-
+DBStorage: database management system (DBMS) uses to create,
+read, update and delete (CRUD) data from a database.
 """
 
-import json
-from models.base_model import BaseModel
+
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import getenv
+from models.base_model import Base
+from sqlalchemy import create_engine
+
+
+class DBStorage:
+    """DBStorage class"""
+    __engine = None
+    __session = None
+
+
+    def __init__(self):
+        # mysql is the dialect and mysqldb is the driver
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')))
+        if getenv('HBNB_MYSQL_ENV') == "test":
+            Base.metadata.drop_all(self.__engine)
+
+
+    def delete(self, obj=None)
+        """
+         delete obj from the current database
+        """
+        if obj:
+            self.__session.delete(obj)
+
+    def save(self):
+        """commits changes"""
+        self.__session.commit()
+
+
+
+
+
+
+
