7b907910aff60ff45b5c9bcfadc42ecba3c2c1d4
David Rosero C <davidroserocalle@gmail.com>
2019-12-18 17:01:13 +0000

    update fike state.py and waith to the db_storage.py file

--- models/state.py
+++ models/state.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    
+    name = Column(String(128), nullable=False)
+
+   if os.getenv("HBNB_TYPE_STORAGE") == "db":
+       
