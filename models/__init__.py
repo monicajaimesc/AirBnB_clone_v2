842c8a71d185c0661977cb79fef04ed002d68b65
monicajaimesc <monikmaja@gmail.com>
2019-12-18 21:54:28 +0000

    init added

--- models/__init__.py
+++ models/__init__.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from os import getenv
-
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine.db_storage import DBStorage
+    # from models.engine.db_storage import DBStorage
     # create an instance of DBSstorage store it in storage
     storage = DBStorage()
     storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
+    # from models.engine.file_storage import FileStorage
     # create an instance of FileStorage and store it in the variable storage
     storage = FileStorage()
     storage.reload()
