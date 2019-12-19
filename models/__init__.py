db12415178a623d34c6997e7078300de9225d117
David Rosero C <davidroserocalle@gmail.com>
2019-12-19 18:46:20 +0000

    pass the import in the conditionals

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from os import getenv
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
 from models.city import City
@@ -11,10 +9,10 @@ from models.place import Place
 from models.review import Review
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    # from models.engine.db_storage import DBStorage
-    # create an instance of DBSstorage store it in storage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload()
