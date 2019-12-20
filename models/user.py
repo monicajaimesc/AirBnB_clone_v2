9e9a7f669fabda413a8f078974a7b340f2cb5fdf
David Rosero C <davidroserocalle@gmail.com>
2019-12-20 15:37:47 +0000

    create a relationship with review in users

--- models/user.py
+++ models/user.py
@@ -19,4 +19,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade="all,delete", backref="user")
+    places = relationship("Place", cascade="all, delete", backref="my_users")
+    reviews = relationship("Review", cascade="all, delete", backref="my_users")
