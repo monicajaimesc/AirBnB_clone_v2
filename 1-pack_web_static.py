a656ed4fc85309d7895ce6c0a22953420124512b
monicajaimesc <monikmaja@gmail.com>
2020-01-06 09:11:40 -0500

    folder tar created

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,17 @@
 from fabric.api import *
 from datetime import datetime
 
-n = datetime.now()
+x = datetime.now()
 
 
 def do_pack():
     """Packs web_static files into .tgz file"""
 
     file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
-                .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
+                .format(x.year, x.month, x.day, x.hour, x.minute, x.second)
     local('mkdir -p versions')
+    # Compress files using tar:
+    # tar -cvzf <name of tarball>.tgz /path/to/source/folder
     command = local("tar -cvzf " + file_name + " ./web_static/")
     if command.succeeded:
         return file_name
