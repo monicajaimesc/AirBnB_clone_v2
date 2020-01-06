f6eee03f704712d40781123c71a7e6586e1c9407
monicajaimesc <monikmaja@gmail.com>
2020-01-06 00:06:35 -0500

    check in docker, working fine

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Generates a .tgz archive from the contents of the web_static folder
+
+from fabric.api import *
+from datetime import datetime
+
+n = datetime.now()
+
+
+def do_pack():
+    """Packs web_static files into .tgz file"""
+
+    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
+                .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
+    local('mkdir -p versions')
+    command = local("tar -cvzf " + file_name + " ./web_static/")
+    if command.succeeded:
+        return file_name
+    else:
+        return None
