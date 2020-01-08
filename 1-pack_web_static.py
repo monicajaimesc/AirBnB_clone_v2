06c1574d4e33855feee7f1e756a9dd1ab14fcf7e
monicajaimesc <monikmaja@gmail.com>
2020-01-08 19:34:02 +0000

    tgz file modificate

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,19 +4,19 @@
 from fabric.api import *
 from datetime import datetime
 
-x = datetime.now()
+time = datetime.now()
 
 
 def do_pack():
-    """Packs web_static files into .tgz file"""
+    """do Packs to create a .tgz file"""
 
     file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
-                .format(x.year, x.month, x.day, x.hour, x.minute, x.second)
+                .format(time.year, time.month, time.day, time.hour, time.minute, time.second)
     local('mkdir -p versions')
     # Compress files using tar:
     # tar -cvzf <name of tarball>.tgz /path/to/source/folder
-    command = local("tar -cvzf " + file_name + " ./web_static/")
-    if command.succeeded:
+    compress = local("tar -cvzf " + file_name + " ./web_static/")
+    if compress.succeeded:
         return file_name
     else:
         return None
