e63ae49dd9f66a2ba80d109b01a24274b8ec6b61
monicajaimesc <monikmaja@gmail.com>
2020-01-08 22:25:46 +0000

    set up working

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # Sets up web servers for the deployment of web_static
 
-apt-get update
-apt-get -y install nginx
+apt-get update -y
+apt-get install nginx -y
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/{releases/test,shared}
+# the file wile contain: 
 echo "<html>
   <head>
   </head>
@@ -12,8 +13,8 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
+# ln to create a symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
-exit 0
