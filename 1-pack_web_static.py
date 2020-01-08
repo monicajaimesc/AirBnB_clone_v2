2cbb0420d28fedeaa977402e9abb57ea0418cdb4
monicajaimesc <monikmaja@gmail.com>
2020-01-08 22:03:27 +0000

    file added

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 # Generates a .tgz archive from the contents of the web_static folder
 
-from datetime import datetime
 from fabric.api import *
-
+from datetime import datetime
 
 time = datetime.now()
 
+
 def do_pack():
     """do Packs to create a .tgz file"""
-    
+
     file_ = 'versions/web_static_{}{}{}{}{}{}.tgz'\
                 .format(time.year, time.month, time.day, time.hour, time.minute, time.second)
     local('mkdir -p versions')
     # Compress files using tar:
     # tar -cvzf <name of tarball>.tgz /path/to/source/folder
-    compress = local("tar -cvzf " + file_name + " ./web_static/")
+    compress = local("tar -cvzf " + file_ + " ./web_static/")
     if compress.succeeded:
         return file_
     else:
