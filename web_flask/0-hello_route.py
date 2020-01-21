29ccb6f391a89dc64d95bd5f20b1aaa29f774f68
monicajaimesc <monikmaja@gmail.com>
2020-01-21 17:20:23 +0000

    fist files added

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""""
+
+from flask import Flask
+
+# flask class, name to identify the current app
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def Hello_route():
+    return "Hello HBNB!"
+
+# application to handle remote requests
+app.run(host='0.0.0.0', port=5000)
+
