857bb7ea3118b5ac159ce6247460aa67eb3c486f
monicajaimesc <monikmaja@gmail.com>
2020-01-21 13:24:41 -0500

    comments added

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,15 +5,17 @@ script that starts a Flask web application
 
 from flask import Flask
 
-# flask class, name to identify the current app
+# flask class, name to identify the current app, create app
 app = Flask(__name__)
 
-
+# specify URL
 @app.route("/", strict_slashes=False)
 def Hello_route():
+    # what that URL returns
     return "Hello HBNB!"
 
 # application to handle remote requests
+# run (app) this code only if I ran it.
 
 
 if __name__ == '__main__':
