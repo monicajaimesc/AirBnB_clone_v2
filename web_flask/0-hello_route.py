ee4b8affd3db7bbc7ee324dbec2c043706a1d685
monicajaimesc <monikmaja@gmail.com>
2020-01-21 12:42:11 -0500

    pycodestyle done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,14 +8,14 @@ from flask import Flask
 # flask class, name to identify the current app
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
 
+@app.route("/", strict_slashes=False)
 def Hello_route():
     return "Hello HBNB!"
 
 # application to handle remote requests
 
+
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
     app.run()
-
