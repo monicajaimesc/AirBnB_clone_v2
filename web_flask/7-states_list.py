1846f6009fa16ba7ff889854d17ee10fe5f035f6
monicajaimesc <monikmaja@gmail.com>
2020-01-22 14:32:12 -0500

    states list showed as html file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+in this file SQLALchemy is gonna be used to establishes conversations
+between the program and the databases.
+"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    function that will display the states list
+    :return: display HTML with the stated sorted
+    """
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    """
+    close the database
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run()
