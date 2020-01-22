e3ea4de7028319ea36f828f4b759cc3425a9993b
monicajaimesc <monikmaja@gmail.com>
2020-01-22 12:43:35 -0500

    stated fixed with the import in storage

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This is the state class"""
+import models
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-import os
-import models
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -15,6 +15,23 @@ class State(BaseModel, Base):
     """
 
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="my_state")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              backref="my_state")
+    else:
+        name = ""
 
+    @property
+    def cities(self):
+        """
+        property method to get the list of cities
+        :return: list of cities (instance) of the states
+        """
+        list_cities = []
+        # city is an object and will be store in store_objects
+        store_objects = models.storage.all(City)
+        for key, city in store_objects.items():
+            if city.state_id == self.id:
+                list_cities.append(city)
+        return list_cities
