03906946596b19bccac02f4b52db5b374f5ef597
monicajaimesc <monikmaja@gmail.com>
2020-01-22 15:56:49 -0500

    cities by state sorted

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+
+"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def city_by_state():
+    """
+    function that will display the cities by state
+    :return: display HTML with the state and it respective cities (sorted)
+    """
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    """
+    close the database
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run()
